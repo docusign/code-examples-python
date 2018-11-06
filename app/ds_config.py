30ea5c1266314f650e088eead6eeb4e28658a165
larry-kluger <larry.kluger@docusign.com>
2018-11-06 22:00:19 +0200

    Update app/ds_config.py

---
+++ app/ds_config.py
@@ -0,0 +1,28 @@
+# ds_config.py
+#
+# DocuSign configuration settings
+import os
+
+DS_CONFIG = {
+    'ds_client_id': '{CLIENT_ID}', # The app's DocuSign integration key
+    'ds_client_secret': '{CLIENT_SECRET}', # The app's DocuSign integration key's secret
+    'signer_email': '{USER_EMAIL}',
+    'signer_name': '{USER_FULLNAME}',
+    'app_url': '{APP_URL}', # The url of the application. Eg http:#localhost:5000
+    # NOTE: You must add a Redirect URI of appUrl/ds/callback to your Integration Key.
+    #       Example: http:#localhost:5000/ds/callback
+    'authorization_server': 'https://account-d.docusign.com',
+    'session_secret': '{SESSION_SECRET}', # Secret for encrypting session cookie content
+    'allow_silent_authentication': True, # a user can be silently authenticated if they have an
+    # active login session on another tab of the same browser
+    'target_account_id': None, # Set if you want a specific DocuSign AccountId, If null, the user's default account will be used.
+    'demo_doc_path': 'demo_documents',
+    'doc_docx': 'World_Wide_Corp_Battle_Plan_Trafalgar.docx',
+    'doc_pdf':  'World_Wide_Corp_lorem.pdf',
+    # Payment gateway information is optional
+    'gateway_account_id': '{DS_PAYMENT_GATEWAY_ID}',
+    'gateway_name': "stripe",
+    'gateway_display_name': "Stripe",
+    'github_example_url': 'https:#github.com/docusign/eg-03-python-auth-code-grant/tree/master/lib/examples/',
+    'documentation': None
+}
