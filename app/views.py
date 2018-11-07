892fadbc4d1c4cf5db99286c707575b55308d0de
larry-kluger <larry.kluger@docusign.com>
2018-11-07 09:42:06 +0200

    get user info; display user & account; flash msgs

--- app/views.py
+++ app/views.py
@@ -1,11 +1,11 @@
-from flask import render_template, url_for, redirect, session
+from flask import render_template, url_for, redirect, session, flash
 from flask_oauthlib.client import OAuth
 from app import app
 from app import ds_config
 from datetime import datetime, timedelta
 import requests
 
-
+base_uri_suffix = '/restapi'
 oauth = OAuth(app)
 docusign = oauth.remote_app(
     'docusign',
@@ -27,9 +27,16 @@ def login():
 
 @app.route('/logout')
 def logout():
-    session.pop('docusign_access_token', None)
-    session.pop('docusign_refresh_token', None)
-    session.pop('docusign_expiration', None)
+    # remove the keys and their values from the session
+    session.pop('ds_access_token', None)
+    session.pop('ds_refresh_token', None)
+    session.pop('ds_user_email', None)
+    session.pop('ds_user_name', None)
+    session.pop('ds_expiration', None)
+    session.pop('ds_account_id', None)
+    session.pop('ds_account_name', None)
+    session.pop('ds_basePath', None)
+
     return redirect(url_for('index'))
 
 
@@ -42,23 +49,40 @@ def ds_callback():
             request.args['error_description'],
             resp
         )
-    session['docusign_access_token'] = resp['access_token']
-    session['docusign_refresh_token'] = resp['refresh_token']
-    session['docusign_expiration'] = datetime.utcnow() + timedelta(seconds=resp['expires_in'])
+    # app.logger.info('Authenticated with DocuSign.')
+    flash('You have authenticated with DocuSign.')
+    session['ds_access_token'] = resp['access_token']
+    session['ds_refresh_token'] = resp['refresh_token']
+    session['ds_expiration'] = datetime.utcnow() + timedelta(seconds=resp['expires_in'])
 
     # Determine user, account_id, base_url by calling OAuth::getUserInfo
     # See https://developers.docusign.com/esign-rest-api/guides/authentication/user-info-endpoints
     url = ds_config.DS_CONFIG['authorization_server'] + '/oauth/userinfo'
-    auth = {"Authorization": "Bearer " + session['docusign_access_token']}
+    auth = {"Authorization": "Bearer " + session['ds_access_token']}
     response = requests.get(url, headers=auth).json()
+    session['ds_user_name'] = response["name"]
+    session['ds_user_email'] = response["email"]
+    accounts = response["accounts"]
+    account = None # the account we want to use
+    # Find the account...
+    target_account_id = ds_config.DS_CONFIG['target_account_id']
+    if target_account_id:
+        account = next( (a for a in accounts if a["account_id"] == target_account_id), None)
+        if not account:
+            # Panic! The user does not have the targeted account. They should not log in!
+            raise Exception("No access to target account")
+    else:
+        account = next((a for a in accounts if a["is_default"]), None)
+        if not account:
+            # Panic! Every user should always have a default account
+            raise Exception("No default account")
+
+    # Save the account information
+    session['ds_account_id'] = account["account_id"]
+    session['ds_account_name'] = account["account_name"]
+    session['ds_basePath'] = account["base_uri"] + base_uri_suffix
 
     return redirect(url_for('index'))
-
-
-@docusign.tokengetter
-def get_docusign_oauth_token():
-    return session.get('docusign_token')
-
 
 
 @app.route('/')
@@ -68,7 +92,6 @@ def index():
 @app.route('/index')
 def r_index():
     return redirect(url_for('index'))
-
 
 
 
