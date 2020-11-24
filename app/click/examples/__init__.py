62d95f7ef80bed90d832096ac6471ca0ea1c5bd9
Olena Harkusha <elena.garkusha1206@gmail.com>
2020-11-24 20:05:55 +0200

    Add get a list of clickwraps example

--- app/click/examples/__init__.py
+++ app/click/examples/__init__.py
@@ -1,2 +1,3 @@
 from .eg001_create_clickwrap import eg001
-from  .eg002_activate_clickwrap import eg002
+from .eg002_activate_clickwrap import eg002
+from .eg006_list_clickwraps import eg006
---
+++ app/click/examples/eg006_list_clickwraps/views.py
@@ -0,0 +1,55 @@
+"""Example 006: Getting a list of clickwraps"""
+
+from os import path
+import json
+
+from docusign_click.client.api_exception import ApiException
+from flask import render_template, current_app, Blueprint, session
+
+from .controller import Eg006Controller
+from app.docusign import authenticate
+from app.ds_config import DS_CONFIG
+from app.error_handlers import process_error
+
+eg = "eg006"  # reference (and url) for this example
+eg006 = Blueprint("eg006", __name__)
+
+
+@eg006.route("/eg006", methods=["POST"])
+@authenticate(eg=eg)
+def clickwrap_list():
+    """
+    1. Get required arguments
+    2. Call the worker method
+    3. Render the response
+    """
+    # 1. Get required arguments
+    args = Eg006Controller.get_args()
+
+    try:
+        # 2. Call the worker method to get a list of clickwraps
+        results = Eg006Controller.worker(args)
+    except ApiException as err:
+        return process_error(err)
+
+    # 3. Render the response
+    return render_template(
+        "example_done.html",
+        title="List clickwraps results",
+        h1="List clickwraps results",
+        message="Results from the ClickWraps::getClickwraps method:",
+        json=json.dumps(json.dumps(results.to_dict(), default=str))
+    )
+
+
+@eg006.route("/eg006", methods=["GET"])
+@authenticate(eg=eg)
+def get_view():
+    """responds with the form for the example"""
+    return render_template(
+        "eg006_list_clickwraps.html",
+        title="Getting a list of clickwraps",
+        source_file=path.basename(path.dirname(__file__)) + "/controller.py",
+        source_url=DS_CONFIG["github_example_url"] + path.basename(
+            path.dirname(__file__)) + "/controller.py",
+    )
