7971789ffd306414e235ec02f22e26878e1d9a2c
Olena Harkusha <elena.garkusha1206@gmail.com>
2020-11-25 18:21:32 +0200

    Add create a new clickwrap version example

--- app/__init__.py
+++ app/__init__.py
@@ -46,6 +46,7 @@ if EXAMPLES_API_TYPE["Rooms"]:
 elif EXAMPLES_API_TYPE["Click"]:
     app.register_blueprint(click_examples.eg001)
     app.register_blueprint(click_examples.eg002)
+    app.register_blueprint(click_examples.eg005)
     app.register_blueprint(click_examples.eg006)
 else:
     app.register_blueprint(examples.eg001)
--- app/click/examples/__init__.py
+++ app/click/examples/__init__.py
@@ -1,3 +1,4 @@
 from .eg001_create_clickwrap import eg001
 from .eg002_activate_clickwrap import eg002
+from .eg005_create_new_clickwrap_version import eg005
 from .eg006_list_clickwraps import eg006
