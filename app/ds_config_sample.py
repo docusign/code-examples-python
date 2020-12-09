3dd98ed2216e61509f5ed59a2e5161323c7eeb04
Paige Rossi <paige.rossi@docusign.com>
2020-12-09 10:13:49 -0800

    fixing issue with JWT authentication location of private.key file

--- app/ds_config_sample.py
+++ app/ds_config_sample.py
@@ -32,7 +32,7 @@ DS_CONFIG = {
 DS_JWT = {
     "ds_client_id": "{INTEGRATION_KEY_JWT}",
     "ds_impersonated_user_id": "{IMPERSONATED_USER_ID}",  # The id of the user.
-    "private_key_file": "./private.key", # Create a new file in your repo source folder named private.key then copy and paste your RSA private key there and save it.
+    "private_key_file": "./app/private.key", # Create a new file in your repo source folder named private.key then copy and paste your RSA private key there and save it.
     "authorization_server": "account-d.docusign.com"
 }
 
