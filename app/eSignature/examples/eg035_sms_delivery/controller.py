b91aed96da0c27f2271ebacea5288b5272afbc02
Paige Rossi <paige.rossi@docusign.com>
2021-01-12 11:57:48 -0800

    cc sms notification

--- app/eSignature/examples/eg035_sms_delivery/controller.py
+++ app/eSignature/examples/eg035_sms_delivery/controller.py
@@ -18,12 +18,18 @@ class Eg035Controller:
         # Strip anything other than characters listed
         signer_email = pattern.sub("", request.form.get("signer_email"))
         signer_name = pattern.sub("", request.form.get("signer_name"))
+        cc_email = pattern.sub("", request.form.get("cc_email"))
+        cc_name = pattern.sub("", request.form.get("cc_name"))
+        cc_phone_number = request.form.get("cc_phone_number")
         phone_number = request.form.get("phone_number")
         envelope_args = {
             "signer_email": signer_email,
             "signer_name": signer_name,
             "status": "sent",
-            "phone_number": phone_number
+            "cc_email": cc_email,
+            "cc_name": cc_name,
+            "phone_number": phone_number,
+            "cc_phone_number": cc_phone_number
         }
         args = {
             "account_id": session["ds_account_id"],
@@ -74,12 +80,12 @@ class Eg035Controller:
         # The envelope will be sent first to the signer.
         # After it is signed, a copy is sent to the cc person.
 
-        # create the envelope definition
+        # Create the envelope definition
         env = EnvelopeDefinition(
             email_subject="Please sign this document set"
         )
         doc1_b64 = base64.b64encode(bytes(cls.create_document1(args), "utf-8")).decode("ascii")
-        # read files 2 and 3 from a local directory
+        # Read files 2 and 3 from a local directory
         # The reads could raise an exception if the file is not available!
         with open(path.join(demo_docs_path, DS_CONFIG["doc_docx"]), "rb") as file:
             doc2_docx_bytes = file.read()
@@ -89,23 +95,23 @@ class Eg035Controller:
         doc3_b64 = base64.b64encode(doc3_pdf_bytes).decode("ascii")
 
         # Create the document models
-        document1 = Document(  # create the DocuSign document object
+        document1 = Document(  # Create the DocuSign document object
             document_base64=doc1_b64,
-            name="Order acknowledgement",  # can be different from actual file name
-            file_extension="html",  # many different document types are accepted
-            document_id="1"  # a label used to reference the doc
+            name="Order acknowledgement",  # Can be different from actual file name
+            file_extension="html",  # Many different document types are accepted
+            document_id="1"  # A label used to reference the doc
         )
-        document2 = Document(  # create the DocuSign document object
+        document2 = Document(  # Create the DocuSign document object
             document_base64=doc2_b64,
-            name="Battle Plan",  # can be different from actual file name
-            file_extension="docx",  # many different document types are accepted
-            document_id="2"  # a label used to reference the doc
+            name="Battle Plan",  # Can be different from actual file name
+            file_extension="docx",  # Many different document types are accepted
+            document_id="2"  # A label used to reference the doc
         )
-        document3 = Document(  # create the DocuSign document object
+        document3 = Document(  # Create the DocuSign document object
             document_base64=doc3_b64,
-            name="Lorem Ipsum",  # can be different from actual file name
-            file_extension="pdf",  # many different document types are accepted
-            document_id="3"  # a label used to reference the doc
+            name="Lorem Ipsum",  # Can be different from actual file name
+            file_extension="pdf",  # Many different document types are accepted
+            document_id="3"  # A label used to reference the doc
         )
         # The order in the docs array determines the order in the envelope
         env.documents = [document1, document2, document3]
@@ -128,14 +134,35 @@ class Eg035Controller:
             routing_order="1",
             additional_notifications=[sms_notification]
         )
+
+        # Create a RecipientPhoneNumber and add it to the additional SMS notfication
+        ccPhoneNumber = RecipientPhoneNumber(
+            country_code="1",
+            number=args["cc_phone_number"]
+        )
+
+        cc_sms_notification = RecipientAdditionalNotification(
+            phone_number=ccPhoneNumber
+        )
+        cc_sms_notification.secondary_delivery_method = "SMS"
+
+        # Create a cc recipient to receive a copy of the documents
+        cc1 = CarbonCopy(
+            email=args["cc_email"],
+            name=args["cc_name"],
+            recipient_id="2",
+            routing_order="2",
+            additional_notifications=[cc_sms_notification]
+        )
+
         # routingOrder (lower means earlier) determines the order of deliveries
         # to the recipients. Parallel routing order is supported by using the
         # same integer as the order for two or more recipients.
 
-        #signer1.additional_notifications = [sms_notification]
+        # signer1.additional_notifications = [sms_notification]
 
         # Create signHere fields (also known as tabs) on the documents,
-        # We"re using anchor (autoPlace) positioning
+        # We're using anchor (autoPlace) positioning
         #
         # The DocuSign platform searches throughout your envelope"s
         # documents for matching anchor strings. So the
@@ -160,10 +187,10 @@ class Eg035Controller:
         signer1.tabs = Tabs(sign_here_tabs=[sign_here1, sign_here2])
 
         # Add the recipients to the envelope object
-        recipients = Recipients(signers=[signer1])
+        recipients = Recipients(signers=[signer1], carbon_copies=[cc1])
         env.recipients = recipients
 
-        # Request that the envelope be sent by setting |status| to "sent".
+        # Request that the envelope be sent by setting status to "sent".
         # To request that the envelope be created as a draft, set to "created"
         env.status = args["status"]
 
@@ -187,6 +214,7 @@ class Eg035Controller:
               color: darkblue;">Order Processing Division</h2>
             <h4>Ordered by {args["signer_name"]}</h4>
             <p style="margin-top:0em; margin-bottom:0em;">Email: {args["signer_email"]}</p>
+            <p style="margin-top:0em; margin-bottom:0em;">Copy to: {args["cc_name"]}, {args["cc_email"]}</p>
             <p style="margin-top:3em;">
                 Candy bonbon pastry jujubes lollipop wafer biscuit biscuit. Topping brownie sesame snaps sweet roll pie. 
                 Croissant danish biscuit soufflé caramels jujubes jelly. Dragée danish caramels lemon drops dragée. 
