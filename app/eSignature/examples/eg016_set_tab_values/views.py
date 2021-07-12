fe9eaaba8c88e6a5725d16cf99bb7674f6443456
Matthew Lusher <matthew.lusher@docusign.com>
2021-07-12 19:41:45 -0400

    Updating links to DevCenter

--- app/eSignature/examples/eg016_set_tab_values/views.py
+++ app/eSignature/examples/eg016_set_tab_values/views.py
@@ -15,7 +15,7 @@ eg016 = Blueprint("eg016", __name__)
 
 
 # the signer? See the "authenticationMethod" definition
-# https://developers.docusign.com/esign-rest-api/reference/Envelopes/EnvelopeViews/createRecipient
+# https://developers.docusign.com/docs/esign-rest-api/reference/envelopes/envelopeviews/createrecipient/
 
 
 @eg016.route("/eg016", methods=["POST"])
