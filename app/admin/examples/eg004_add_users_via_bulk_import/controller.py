0641bf94f8b65b6b9cfbe885968d090a1457f222
Paige Rossi <paige.rossi@docusign.com>
2021-08-24 15:24:31 -0700

    fixing csv

--- app/admin/examples/eg004_add_users_via_bulk_import/controller.py
+++ app/admin/examples/eg004_add_users_via_bulk_import/controller.py
@@ -56,6 +56,6 @@ class Eg004Controller:
         # Returns an example of a CSV file
         return (
             "AccountID,UserName,UserEmail,PermissionSet\n"
-            {account_id},First1,Last1,example1@sampleemail.example,DS Admin
-            {account_id},First2,Last2,example2@sampleemail.example,DS Admin
+            "{account_id},First1,Last1,example1@sampleemail.example,DS Admin"
+            "{account_id},First2,Last2,example2@sampleemail.example,DS Admin"
         )
