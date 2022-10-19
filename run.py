1549fa5d4c82de42b98ba30aa1161f06ae831389
Paige Rossi <70241979+paigesrossi@users.noreply.github.com>
2022-10-19 11:13:15 -0700

    updating pipenv version to earliest fixed version (#93)
    * updating pipenv version to earliest fixed version
    
    * click package update

--- run.py
+++ run.py
@@ -1,11 +1,18 @@
 #!flask/bin/python
 from app import app
+from flask_session import Session
 import os
 
 if os.environ.get("DEBUG", False) == "True":
     app.config["DEBUG"] = True
+    app.config['SESSION_TYPE'] = 'filesystem'
+    sess = Session()
+    sess.init_app(app)
     port = int(os.environ.get("PORT", 3000))
     app.run(host="localhost", port=3000, debug=True)
 else:
+    app.config['SESSION_TYPE'] = 'filesystem'
+    sess = Session()
+    sess.init_app(app)
     app.run(host="localhost", port=3000, extra_files="api_type.py")
 
