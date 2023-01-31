3650e42fee7bb71a3b755c2476c4d392d31233c0
Paige Rossi <paige.rossi@docusign.com>
2023-01-31 09:53:44 -0800

    reverting quickstart sample config change

--- app/ds_config_sample.py
+++ app/ds_config_sample.py
@@ -2,18 +2,13 @@
 #
 # DocuSign configuration settings
 
-import os
-from dotenv import load_dotenv
-
-
-load_dotenv()
-
 DS_CONFIG = {
-    "ds_client_id": os.environ.get("CLIENT_ID"),  # The app's DocuSign integration key
-    "ds_client_secret": os.environ.get("CLIENT_SECRET"),  # The app's DocuSign integration key's secret
-    "signer_email": os.environ.get("SIGNER_EMAIL"),
-    "signer_name": os.environ.get("SIGNER_NAME"),
-    "app_url": "http://localhost:5000",  # The URL of the application. Eg http://localhost:5000
+    "ds_client_id": "{INTEGRATION_KEY_AUTH_CODE}",  # The app's DocuSign integration key
+    "ds_client_secret": "{SECRET_KEY}",  # The app's DocuSign integration key's secret
+    "organization_id": "{ORGANIZATION_ID}", # A GUID value that identifies the organization
+    "signer_email": "{SIGNER_EMAIL}",
+    "signer_name": "{SIGNER_NAME}",
+    "app_url": "http://localhost:3000",  # The URL of the application. Eg http://localhost:5000
     # NOTE: You must add a Redirect URI of appUrl/ds/callback to your Integration Key.
     #       Example: http://localhost:5000/ds/callback
     "authorization_server": "https://account-d.docusign.com",
@@ -32,22 +27,21 @@ DS_CONFIG = {
     "doc_terms_pdf": "Term_Of_Service.pdf",
     "doc_txt": "Welcome.txt",
     # Payment gateway information is optional
-    "gateway_account_id": os.environ.get("DS_PAYMENT_GATEWAY_ID"),
-    "gateway_name": os.environ.get("DS_PAYMENT_GATEWAY_NAME"),
-    "gateway_display_name": os.environ.get("DS_PAYMENT_GATEWAY_DISPLAY_NAME"),
+    "gateway_account_id": "{DS_PAYMENT_GATEWAY_ID}",
+    "gateway_name": "stripe",
+    "gateway_display_name": "Stripe",
     "github_example_url": "https://github.com/docusign/code-examples-python/tree/master/app/eSignature/examples/",
     "monitor_github_url": "https://github.com/docusign/code-examples-python/tree/master/app/monitor/examples/",
     "admin_github_url": "https://github.com/docusign/code-examples-python/tree/master/app/admin/examples/",
     "click_github_url": "https://github.com/docusign/code-examples-python/tree/master/app/click/examples/",
     "example_manifest_url": "https://raw.githubusercontent.com/docusign/code-examples-csharp/master/manifest/CodeExamplesManifest.json",
     "documentation": "",  # Use an empty string to indicate no documentation path.
-    "quickstart": os.environ.get("QUICKSTART_VALUE"),
-    "quick_acg": False
+    "quickstart": "{QUICKSTART_VALUE}"
 }
 
 DS_JWT = {
-    "ds_client_id": os.environ.get("CLIENT_ID"),
-    "ds_impersonated_user_id": os.environ.get("USER_ID"),  # The id of the user.
+    "ds_client_id": "{INTEGRATION_KEY_JWT}",
+    "ds_impersonated_user_id": "{IMPERSONATED_USER_ID}",  # The id of the user.
     "private_key_file": "./app/private.key", # Create a new file in your repo source folder named private.key then copy and paste your RSA private key there and save it.
     "authorization_server": "account-d.docusign.com"
 }
